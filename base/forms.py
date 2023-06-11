7ac7578d89a12a32472d66ef85b5169e90912344
Datta Kiran Rao <dattakiranrao@192.168.1.3>
2023-06-11 18:36:48 +0530

    Finished The Project

--- base/forms.py
+++ base/forms.py
@@ -1,14 +1,22 @@
 from django.forms import ModelForm
 from .models import Room, User
+from django.contrib.auth.forms import (
+    UserCreationForm,
+)  # used to set 2 or more search parameter in one check
+
+class MyUserCreationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['name', 'username', 'email', 'password1', 'password2']
 
 class RoomForm(ModelForm):
     class Meta:
         model = Room
         fields = "__all__"  # all will create the exact form with all the components mentioned in the model
-        exclude = ['host', 'participants']
+        exclude = ["host", "participants"]
 
 
 class UserForm(ModelForm):
     class Meta:
-        model = User 
-        fields = ['username', 'email']
+        model = User
+        fields = ["avatar", "name", "username", "email", "bio"]
--- base/models.py
+++ base/models.py
@@ -6,10 +6,9 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     name = models.CharField(max_length=100, null=True)
-    email = models.EmailField(max_length=100, null=True)
+    email = models.EmailField(max_length=100, null=True, unique=True)
     bio = models.TextField(null=True)
-
-    # avatar = 
+    avatar = models.ImageField(null=True, default="avatar.svg")
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
